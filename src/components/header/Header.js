b14eb98aabffdfce2de4fe42e6fa2f8121518748
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-17 15:38:50 +0100

    add main structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <h1 className='header__heading'>The Horned Beast app</h1>
+    </div>
+  )
+}
