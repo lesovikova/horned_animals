4b12bc7510c0841447ccbf390e575ea1fb64ee08
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-19 08:30:25 +0100

    add useState to beasts and footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 
 export default function Footer() {
+
+let [name, changed] = useState("Anna");
+
+function changeName() {
+  changed(name="Ann") 
+}
+
   return (
-    <div className='footer'>
-      <p className='footer__copy'>2023 Anna Lisovykova</p>
+    <div onClick={changeName} className='footer'>
+      <p className='footer__copy'>2023 {name} Lisovykova</p>
     </div>
   )
 }
--- src/components/hornedBeast/HornedBeast.js
+++ src/components/hornedBeast/HornedBeast.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HornedBeast.css';
 
-export default function HornedBeast(props) {
+
+
+export default function HornedBeast({title, image_url, keyword, description}) {
+  
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count+1);
+  }
+
   return (
     <div className='beast'>
-        <h2 className='beast__title'>{props.title}</h2>
-        <img className='beast__image' src={props.image_url} alt={props.keyword}></img>
-        <p className='beast__description'>{props.description}</p>
+        <h2 className='beast__title'>{title}</h2>
+        <img onClick={increaseCount} className='beast__image' src={image_url} alt={keyword}></img>
+        <p className='beast__description'>{description}</p>
+        <p className='beast__counter'>This beast is favourite {count} times</p>
     </div>
   )
 }
